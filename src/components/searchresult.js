ad1d2e6ba20b768a21c7ae20c54e38c865a7c6da
alaaet <alaaet@gmail.com>
2020-08-10 18:10:30 +0200

    fixed search results

--- src/components/searchresult.js
+++ src/components/searchresult.js
@@ -1,51 +1,46 @@
-import React,{Component} from 'react';
-import Home from './Home';
-import {HomeConsumer} from './Context';
-import styled from 'styled-components';
-import NewHome from './newhome';
+import React, { Component } from "react";
+import Home from "./Home";
+import { HomeConsumer } from "./Context";
+import styled from "styled-components";
+import NewHome from "./newhome";
 
+class Result extends Component {
+  render() {
+    var type = this.props.location.state.referrer.typeselect;
+    var place = this.props.location.state.referrer.placeselect;
+    var category = this.props.location.state.referrer.catselect;
 
-class Result extends Component{
-    render(){
-        var type=this.props.location.state.referrer.typeselect;
-        var palce=this.props.location.state.referrer.placeselect;
-        var category=this.props.location.state.referrer.catselect;
-        
-        console.log(palce ,type,category);
-        return(
-          <React.Fragment>
-           
-              <Container>
-            <div className="row ">
-                      <HomeConsumer>
-                      {value=>{
-                                return value.obj.Home.map(items=>{
-                                    if(items.addres===palce && items.type===type&&items.category===category){
-                                        return <NewHome key={items.id} item={items}/>;
-                                    }
-                                    
-                                      
-                                       
-                                   } );
-                                
-                                   
-                               }}
-                      </HomeConsumer>
-
-                  </div>
-             
-         </Container>
-
+    console.log(place, type, category);
+    return (
+      <React.Fragment>
+        <Container>
+          <div className="row ">
+            <HomeConsumer>
+              {(value) => {
+                let filteredItems = value.obj.Home.filter((item) => {
+                  return (
+                    item.addres === place &&
+                    item.type === type &&
+                    item.category === category
+                  );
+                });
+                if (filteredItems.length > 0)
+                  return filteredItems.map((item) => {
+                    return <NewHome key={item.id} item={item} />;
+                  });
+                return <h3 className="w-100 text-center">No results found</h3>;
+              }}
+            </HomeConsumer>
+          </div>
+        </Container>
       </React.Fragment>
-       //
-      
-   )
-}
-
+      //
+    );
+  }
 }
 export default Result;
-const Container=styled.div`
-margin:2rem;
-padding:1rem;
-direction:rtl;
- `
+const Container = styled.div`
+  margin: 2rem;
+  padding: 1rem;
+  direction: rtl;
+`;
